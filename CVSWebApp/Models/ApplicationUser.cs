4fc00156f0f114ba1ae778a189470670dd246550
gulfnetworks <gulfnetworks2017@gmail.com>
2018-01-18 08:49:18 +0300

    Initial commit

---
+++ CVSWebApp/Models/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CVSWebApp.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser<int>
+    {
+        [Display(Name = "User Name")]
+        public override string UserName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Mobile No")]
+        public string MobileNo { get; set; }
+
+        [Display(Name = "Manager ID")]
+        public int ManagerId { get; set; }
+
+        [Display(Name = "Company ID")]
+        public int CompanyId { get; set; }
+        public int Active { get; set; }
+
+        [Display(Name = "Outlet ID")]
+        public int OutletId { get; set; }
+        public virtual ICollection<Outlet> Outlets { get; set; }
+
+    }
+}
---
+++ CVSWebApp/Models/Outlet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CVSWebApp.Models
+{
+    public class Outlet
+    {
+        public int OutletId { get; set; }
+        public string OutletName { get; set; }
+        public string OutletAddress { get; set; }
+        public string OutletCountry { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        
+    }
+}
